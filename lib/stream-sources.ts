f2b4dd7b71b1267fe77cf5fe97d18d900c3ce25c
mvinduwara <mvinduwara@gmail.com>
2026-05-02 23:08:23 +0530

    Add TV stream sources and tidy movie sources
    Split stream providers by media type: keep MOVIE_SOURCES (three providers) and add a new exported TV_SOURCES array. TV_SOURCES includes providers with URL builders that accept id, season (s) and episode (e) parameters for TV episode embeds. Also removed the AutoEmbed entry from the movie list and cleaned up formatting.

--- lib/stream-sources.ts
+++ lib/stream-sources.ts
@@ -1,6 +1,11 @@
 export const MOVIE_SOURCES = [
-    { name: 'Server 1 (VidSrc)', url: (id: string) => `https://vidsrc.me/embed/movie/${id}` },
-    { name: 'Server 2 (2Embed)', url: (id: string) => `https://2embed.cc/embed/${id}` },
-    { name: 'Server 3 (EmbedSU)', url: (id: string) => `https://embedsu.com/embed/movie?tmdb=${id}` },
-    { name: 'Server 4 (AutoEmbed)', url: (id: string) => `https://player.autoembed.cc/embed/movie/${id}` },
-  ];
+  { name: 'Server 1 (VidSrc)', url: (id: string) => `https://vidsrc.me/embed/movie/${id}` },
+  { name: 'Server 2 (2Embed)', url: (id: string) => `https://2embed.cc/embed/${id}` },
+  { name: 'Server 3 (EmbedSU)', url: (id: string) => `https://embedsu.com/embed/movie?tmdb=${id}` },
+];
+
+export const TV_SOURCES = [
+  { name: 'Server 1 (VidSrc)', url: (id: string, s: string, e: string) => `https://vidsrc.me/embed/tv/${id}/${s}/${e}` },
+  { name: 'Server 2 (EmbedSU)', url: (id: string, s: string, e: string) => `https://embedsu.com/embed/tv?tmdb=${id}&s=${s}&e=${e}` },
+  { name: 'Server 3 (AutoEmbed)', url: (id: string, s: string, e: string) => `https://player.autoembed.cc/embed/tv/${id}/${s}/${e}` },
+];
